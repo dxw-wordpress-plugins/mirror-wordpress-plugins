97644eccdd5aeb03ddc4b3bad250c06aa45c8863
Sarah Mount <sarah.mount@dxw.com>
2024-09-30 18:07:10 +0100

    Factor error classes into a mixin
    We're expecting to add at least one new script to this
    repo in the current PR and there is already quite a bit
    of repeated code. Also, the mirror script is too long.
    
    As a first step to help with this, this commit factors
    new error classes into a mixin in the new 'lib/' directory.
    
    Future PRs should try to factor out common code wherever
    it is practical to do so.
    
    This commit should not change the behaviour of either
    of the top-level scripts.

---
+++ lib/error_mixin.rb
@@ -0,0 +1,32 @@
+# Generic error class, can be caught but should not be raised.
+class PluginUpdateError < StandardError
+end
+
+# Could not retrieve information about a given plugin.
+class WordPressPluginApiError < PluginUpdateError
+end
+
+# Could retrieve API information but it included an error.
+class WordPressPluginJSONContainsError < PluginUpdateError
+  attr_reader :reason
+  def initialize(msg = "Error from wordpress.org", reason = "")
+    @reason = reason
+    super(msg)
+  end
+end
+
+# Could not download a .zip file for a given plugin.
+class WordPressPluginDownloadError < PluginUpdateError
+end
+
+# Could not unzip the .zip file for a given plugin.
+class WordPressPluginUnzipError < PluginUpdateError
+end
+
+# Could not perform Git action on repo. Only useful for interacting with remotes.
+class GitError < PluginUpdateError
+end
+
+# Could not find a latest version tag for the plugin
+class MissingVersionError < PluginUpdateError
+end
